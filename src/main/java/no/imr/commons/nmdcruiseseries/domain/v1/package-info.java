5d2a7266fd10554b7572ba76b6d80fd5df0a7338
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-22 15:19:52 +0200

    Fixed generation

--- src/main/java/no/imr/commons/nmdcruiseseries/domain/v1/package-info.java
+++
@@ -1,4 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/nmdcruiseseries/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
-    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/nmdcruiseseries/v1", prefix = "")
-})
-package no.imr.commons.nmdcruiseseries.domain.v1;
